7e5853985008abf5a4b55dafa2e19531f830b187
hyeonjiroh <sonamu6509@naver.com>
2025-03-19 12:36:22 +0900

    chore: Add font and color system configuration

---
+++ docs/STYLE_GUIDE.md
@@ -0,0 +1,44 @@
+## 폰트 사이즈
+
+| Tailwind 클래스          | 크기(px) | 줄 높이(px) |
+| ------------------------ | -------- | ----------- |
+| `text-3xl`               | `32px`   | `42px`      |
+| `text-2xl`               | `24px`   | `32px`      |
+| `text-xl`                | `20px`   | `32px`      |
+| `text-lg`                | `18px`   | `26px`      |
+| `text-md`                | `14px`   | `24px`      |
+| `text-sm`                | `13px`   | `22px`      |
+| `text-xs `               | `12px`   | `20px`      |
+| `text-xs leading-[18px]` | `12px`   | `18px`      |
+
+## 폰트 굵기
+
+| Tailwind 클래스 | 굵기  |
+| --------------- | ----- |
+| `font-bold`     | `700` |
+| `font-semibold` | `600` |
+| `font-medium`   | `500` |
+| `font-normal`   | `400` |
+
+### 색상
+
+| 색상 이름  | HEX 코드  |
+| ---------- | --------- |
+| `white`    | `#FFFFFF` |
+| `black`    | `#000000` |
+| `gray-900` | `#171717` |
+| `gray-800` | `#333236` |
+| `gray-700` | `#4B4B4B` |
+| `gray-600` | `#787486` |
+| `gray-500` | `#9FA6B2` |
+| `gray-400` | `#D9D9D9` |
+| `gray-300` | `#EEEEEE` |
+| `gray-200` | `#FAFAFA` |
+| `violet`   | `#5534DA` |
+| `violet-8` | `#F1EFFD` |
+| `red`      | `#D6173A` |
+| `green`    | `#7AC555` |
+| `purple`   | `#760DDE` |
+| `orange`   | `#FFA500` |
+| `blue`     | `#7A6AEA` |
+| `pink`     | `#E876EA` |
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,58 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pretandard: ["Pretendard", "sans-serif"],
+      },
+      fontSize: {
+        "3xl": ["32px", "42px"],
+        "2xl": ["24px", "32px"],
+        xl: ["20px", "32px"],
+        "2lg": ["18px", "26px"],
+        lg: ["16px", "26px"],
+        md: ["14px", "24px"],
+        sm: ["13px", "22px"],
+        xs: ["12px", "20px"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        gray: {
+          "900": "#171717",
+          "800": "#333236",
+          "700": "#4B4B4B",
+          "600": "#787486",
+          "500": "#9FA6B2",
+          "400": "#D9D9D9",
+          "300": "#EEEEEE",
+          "200": "#FAFAFA",
+        },
+        violet: {
+          DEFAULT: "#5534DA",
+          "8": "#F1EFFD",
+        },
+        red: {
+          DEFAULT: "#D6173A",
+        },
+        green: {
+          DEFAULT: "#7AC555",
+        },
+        purple: {
+          DEFAULT: "#760DDE",
+        },
+        orange: {
+          DEFAULT: "#FFA500",
+        },
+        blue: {
+          DEFAULT: "#7A6AEA",
+        },
+        pink: {
+          DEFAULT: "#E876EA",
+        },
       },
     },
   },
