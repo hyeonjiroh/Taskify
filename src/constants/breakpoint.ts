8628603bdd6276d19aef782838bb7e1b765da49d
hyeonjiroh <sonamu6509@naver.com>
2025-03-20 22:04:16 +0900

    refactor: Use  for BREAKPOINTS to ensure readonly values

---
+++ src/constants/breakpoint.ts
@@ -0,0 +1,7 @@
+const BREAKPOINTS = {
+  MOBILE: 375,
+  TABLET: 768,
+  PC: 1200,
+} as const;
+
+export default BREAKPOINTS;
--- src/utils/breakpoints.ts
+++
@@ -1,7 +0,0 @@
-const BREAKPOINTS: { MOBILE: number; TABLET: number; PC: number } = {
-  MOBILE: 375,
-  TABLET: 768,
-  PC: 1200,
-};
-
-export default BREAKPOINTS;
