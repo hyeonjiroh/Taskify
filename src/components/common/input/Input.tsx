b85e98487d44ee4c98b762cb1a5ced84e72afe3c
ARON-Y <arar0811@gmail.com>
2025-03-22 04:05:57 +0900

    feat: finalize Input component with dynamic styles

---
+++ src/components/common/input/Input.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { ChangeEvent } from "react";
+import { inputStyles } from "@/components/common/input/inputStyles";
+import { usePasswordVisibility } from "@/components/common/input/usePasswordVisibility";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
+
+interface InputProps {
+  type?: "text" | "email" | "password" | "date";
+  label: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+  width?: string;
+  height?: "sm" | "md" | "lg" | "xl";
+  hasIcon?: "left" | "right";
+  iconSrc?: string;
+  alt?: string;
+  error?: boolean;
+  errorMessage?: string;
+}
+
+const Input = ({
+  type = "text",
+  label,
+  placeholder,
+  value,
+  onChange,
+  disabled = false,
+  width = "full",
+  height = "xl",
+  hasIcon,
+  iconSrc,
+  alt,
+  error = false,
+  errorMessage,
+}: InputProps) => {
+  const { isPasswordVisible, togglePasswordVisibility } =
+    usePasswordVisibility();
+
+  const inputType = type === "password" && isPasswordVisible ? "text" : type;
+  const iconPath =
+    type === "password"
+      ? isPasswordVisible
+        ? "/icon/visibility_off_icon.svg"
+        : "/icon/visibility_icon.svg"
+      : iconSrc;
+
+  const styles = twMerge(
+    clsx(inputStyles.base, inputStyles.height[height], {
+      "w-full": width === "full",
+      [inputStyles.leftIcon]: hasIcon === "left",
+      [inputStyles.rightIcon]: hasIcon === "right",
+      [inputStyles.disabled]: disabled,
+      [inputStyles.error]: error,
+    })
+  );
+  return (
+    <div className="flex flex-col">
+      <label className={inputStyles.label} htmlFor={label}>
+        {label}
+      </label>
+      <div
+        className="relative"
+        style={{ width: width !== "full" ? width : undefined }}
+      >
+        <input
+          className={styles}
+          type={inputType}
+          id={label}
+          name={label}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+        />
+        {hasIcon && iconPath && (
+          <Image
+            src={iconPath}
+            alt={alt || "icon"}
+            width={24}
+            height={24}
+            className={clsx("absolute top-1/2 transform -translate-y-1/2", {
+              "left-4": hasIcon === "left",
+              "right-4": hasIcon === "right",
+              "cursor-pointer": type === "password",
+            })}
+            onClick={togglePasswordVisibility}
+          />
+        )}
+      </div>
+      {error && errorMessage && (
+        <span className={clsx(inputStyles.errorMessage)}>{errorMessage}</span>
+      )}
+    </div>
+  );
+};
+
+export default Input;
---
+++ src/components/common/input/inputStyles.tsx
@@ -0,0 +1,15 @@
+export const inputStyles = {
+  base: "box-border w-full px-4 rounded-lg border border-gray-400 text-2lg text-gray-800 placeholder-gray-500 focus:outline-none focus:border focus:border-violet",
+  label: "text-2lg text-gray-800 mb-2",
+  leftIcon: "pl-[46px] pr-4",
+  rightIcon: "pl-4 pr-11",
+  height: {
+    sm: "h-[42px]",
+    md: "h-[46px]",
+    lg: "h-[48px]",
+    xl: "h-[50px]",
+  },
+  disabled: "bg-gray-300 text-gray-600 cursor-not-allowed",
+  error: "border-red",
+  errorMessage: "text-red text-md mt-2",
+};
