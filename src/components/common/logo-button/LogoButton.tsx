b8f58afb18cee6a667c2336bc1f8c135ad6c1fc4
hyeonjiroh <sonamu6509@naver.com>
2025-03-23 01:36:16 +0900

    feat: Make LogoButton navigate different routes based on variant

--- src/components/common/logo-button/LogoButton.tsx
+++ src/components/common/logo-button/LogoButton.tsx
@@ -4,26 +4,23 @@ import SmallLogo from "../../../../public/logo/logo_small.svg";
 import LargeLogo from "../../../../public/logo/logo_large.svg";
 
 export default function LogoButton({
-  isMobile,
   variant,
+  isMobile,
 }: {
+  variant: "white" | "purple";
   isMobile: boolean;
-  variant: "purple" | "white";
 }) {
+  const isHome = variant === "white";
+
   return (
-    <button
-      type="button"
-      className="flex justify-center items-center pc:justify-start"
-    >
-      <Link href="/">
-        <Image
-          src={isMobile ? SmallLogo : LargeLogo}
-          width={isMobile ? 24 : 110}
-          height={isMobile ? 30 : 34}
-          className={variant === "white" ? "invert brightness-0" : ""}
-          alt=""
-        />
-      </Link>
-    </button>
+    <Link href={isHome ? "/" : "/mydashboard"}>
+      <Image
+        src={isMobile ? SmallLogo : LargeLogo}
+        width={isMobile ? 24 : 110}
+        height={isMobile ? 30 : 34}
+        className={isHome ? "invert brightness-0" : ""}
+        alt=""
+      />
+    </Link>
   );
 }
