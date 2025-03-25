71d75c9e79d88e6fd1cfa8deb6326328be85f117
hyeonjiroh <sonamu6509@naver.com>
2025-03-25 22:00:47 +0900

    refactor: Remove isMobile prop from LogoButton

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -9,7 +9,7 @@ export default function Sidebar() {
   return (
     <div className="flex flex-col gap-[39px] tablet:gap-[57px] pc:gap-14">
       <div className="flex justify-center items-center tablet:justify-start">
-        <LogoButton variant={"purple"} isMobile={isMobile} />
+        <LogoButton variant={"purple"} />
       </div>
       <div className="flex flex-col items-center gap-[22px] tablet:gap-[15px] tablet:items-stretch pc:gap-[16px]">
         <div className="flex justify-between">
