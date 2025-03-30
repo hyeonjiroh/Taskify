80d705090d7780782a85da40446810815db9c598
hyeonjiroh <sonamu6509@naver.com>
2025-03-30 20:23:01 +0900

    feat: Add redirect logic based on accessToken presence

--- src/app/(after-login)/mypage/page.tsx
+++ src/app/(after-login)/mypage/page.tsx
@@ -1,8 +1,16 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import BackButton from "./_components/BackButton";
 import PasswordSection from "./_components/PasswordSection";
 import ProfileSection from "./_components/ProfileSection";
 
 export default function Page() {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  if (!accessToken) {
+    redirect("/");
+  }
+
   return (
     <div className="flex flex-col px-3 py-4">
       <div className="flex flex-col gap-[6px] tablet:gap-[29px]">
