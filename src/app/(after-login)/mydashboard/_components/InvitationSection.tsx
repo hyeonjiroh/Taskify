3cbb903d5395a3a6ffd1f96037ce5d2f0b81d1a0
hyeonjiroh <sonamu6509@naver.com>
2025-03-31 23:33:36 +0900

    Feat: Show guide message and image when no invites or search results

--- src/app/(after-login)/mydashboard/_components/InvitationSection.tsx
+++ src/app/(after-login)/mydashboard/_components/InvitationSection.tsx
@@ -5,8 +5,10 @@ import { useIntersection } from "@/lib/hooks/useIntersection";
 import { Invitation } from "@/lib/types";
 import { fetchInvitationList } from "@/lib/apis/invitationsApi";
 import Input from "@/components/common/input/Input";
-import SearchIcon from "../../../../../public/icon/search_icon.svg";
 import InvitationCard from "./InvitationCard";
+import Image from "next/image";
+import SearchIcon from "../../../../../public/icon/search_icon.svg";
+import EmptyImg from "../../../../../public/mydashboardPage/empty_invitation.svg";
 
 const PAGE_SIZE = 6;
 
@@ -70,7 +72,7 @@ export default function InvitationSection({ token }: { token: string }) {
   };
 
   return (
-    <div className="flex flex-col gap-[13px] max-w-[1022px] px-4 py-6 rounded-lg bg-white tablet:gap-6 tablet:px-[28px] tablet:py-[18px] pc:py-8">
+    <div className="flex flex-col gap-[13px] max-w-[1022px] min-h-[327px] px-4 py-6 rounded-lg bg-white tablet:gap-6 tablet:min-h-[390px] tablet:px-[28px] tablet:py-[18px] pc:py-8">
       <div className="flex flex-col gap-4 tablet:gap-[17px] pc:gap-8">
         <h2 className="font-bold text-xl text-gray-800 tablet:text-2xl">
           초대받은 대시보드
@@ -86,23 +88,41 @@ export default function InvitationSection({ token }: { token: string }) {
           placeholder="검색"
         />
       </div>
-      <div className="flex flex-col">
-        <div className="hidden tablet:flex justify-between text-gray-500 text-lg pc:pl-12">
-          <span className="w-1/3">이름</span>
-          <span className="w-1/3">초대자</span>
-          <span className="w-1/3">수락 여부</span>
+      {items.length === 0 && (
+        <div className="flex flex-col flex-1 justify-center items-center gap-4">
+          <div>
+            <Image
+              src={EmptyImg}
+              className="size-[60px] tablet:size-[100px]"
+              alt=""
+            />
+          </div>
+          <div className="font-normal text-xs text-gray-500 tablet:text-2lg">
+            아직 초대받은 대시보드가 없어요
+          </div>
         </div>
-        <div className="flex flex-col">
-          {items.map((item, index) => (
-            <div
-              key={item.id}
-              ref={index === items.length - 1 ? observerRef : null}
-            >
-              <InvitationCard {...item} token={token} />
+      )}
+      {items.length > 0 && (
+        <>
+          <div className="flex flex-col">
+            <div className="hidden tablet:flex justify-between text-gray-500 text-lg pc:pl-12">
+              <span className="w-1/3">이름</span>
+              <span className="w-1/3">초대자</span>
+              <span className="w-1/3">수락 여부</span>
             </div>
-          ))}
-        </div>
-      </div>
+            <div className="flex flex-col">
+              {items.map((item, index) => (
+                <div
+                  key={item.id}
+                  ref={index === items.length - 1 ? observerRef : null}
+                >
+                  <InvitationCard {...item} token={token} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
